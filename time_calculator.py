4ca2bfda31c9a2d7fe749b345bde283e5ff081fb
Miguel T Rivera <mrivera108@outlook.com>
2021-02-11 20:36:12 -0800

    Pass test_different_period test

--- time_calculator.py
+++ time_calculator.py
@@ -14,12 +14,26 @@ def add_time(start, duration, start_day=None):
     duration_hour = int(duration_hour_str)
     duration_minute = int(duration_minute_str)
 
+    runoff_minutes = 0
+    runoff_hour = 0
+    adjusted_hour = 0
+    times_of_day = { 'AM': 'PM', 'PM': 'AM'}
+
     new_hour = start_hour + duration_hour
+    new_minute = start_minute + duration_minute
+
+    if start_minute + duration_minute > 59:
+        runoff_minutes = (start_minute + duration_minute) - 60
+        runoff_hour += 1
+        # If minutes is less than ten, append a leading zero
+        if runoff_minutes < 10:
+            new_minute = '0' + str(runoff_minutes)
 
     if new_hour > 12:
-        pass
-    if start_minute + duration_minute > 59:
-        pass
+        adjusted_hour = new_hour + runoff_hour
+        new_hour = adjusted_hour % 12
 
-    new_minute = start_minute + duration_minute
+        if new_hour % 12 == new_hour:
+            time_of_day = times_of_day[time_of_day]
+
     return str(new_hour) + ":" + str(new_minute) + ' ' + time_of_day
